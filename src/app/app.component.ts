fe0ce9731675de57eb415a30ab93670f4013cd8d
DaliAbess <daliabess51@gmail.com>
2024-05-22 15:25:33 +0000

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CarteVisiteComponent } from './carte-visite/carte-visite.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,CarteVisiteComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Hello team';
+  countNum = 1;
+  count(){
+    this.countNum++;
+  }
+}
